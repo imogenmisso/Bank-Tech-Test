5760c2bc6336c532d710c7c49575cf9eaa8b97b7
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-03 16:20:30 +0000

    Rubocop changes to account spec

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,24 +4,24 @@ require 'account'
 
 describe 'Account' do
   let(:account) { Account.new }
-  let(:time) {Time.now.strftime("%d/%m/%Y")}
+  let(:time) { Time.now.strftime('%d/%m/%Y') }
 
   it 'sets the initial balance of the account to zero' do
     expect(account.balance).to eq 0
   end
 
-  context "#deposit" do
+  context '#deposit' do
     it 'allows users to add money to their account' do
       expect { account.deposit(10) }.to change { account.balance }.by(10)
     end
 
-    it "records the date that a deposit is made" do
+    it 'records the date that a deposit is made' do
       account.deposit(10)
       expect(account.time).to eq time
     end
   end
 
-  context "#withdraw" do
+  context '#withdraw' do
     it 'allows users to withdraw money from their account' do
       account.deposit(5)
       expect { account.withdraw(5) }.to change { account.balance }.by(-5)
@@ -33,7 +33,7 @@ describe 'Account' do
       expect(account.balance).to eq 5
     end
 
-    it "records the date a withdrawal is made" do
+    it 'records the date a withdrawal is made' do
       account.deposit(10)
       account.withdraw(5)
       expect(account.time).to eq time
