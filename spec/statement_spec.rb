1656223b31eed2533c098ed7ca7bcb7060487b85
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-04 12:12:18 +0000

    Refactored statement spec to include doubles

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,7 +4,10 @@ require 'statement'
 
 describe 'Statement' do
   let(:time) { Time.now.strftime('%d/%m/%Y') }
-  let(:account) { double :account, transactions: [[Transaction.new(time, 'credit', '10.00', '10.00')], [Transaction.new(time, 'credit', '100.00', '110.00')], [Transaction.new(time, 'debit', '5.00', '105.00')]] }
+  let(:transaction1) { double :transaction1, date: time, type: "credit", amount: "10.00", balance: "10.00"}
+  let(:transaction2) { double :transaction2, date: time, type: "credit", amount: "100.00", balance: "110.00"}
+  let(:transaction3) { double :transaction3, date: time, type: "debit", amount: "5.00", balance: "105.00"}
+  let(:account) { double :account, transactions: [[transaction1], [transaction2], [transaction3]] }
 
   let(:statement) { Statement.new(account) }
 
