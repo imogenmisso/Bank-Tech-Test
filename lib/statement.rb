3ac978bb59199bd1964381a2f8a5d5df9beadc2f
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-04 12:11:52 +0000

    Updated statement class to allow for new transaction object

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,16 +1,30 @@
 # frozen_string_literal: true
-
+require 'pry'
 # This class is responsible for printing all the transactions
 class Statement
-  def initialize(account = Account.new)
+  def initialize(account)
     @account = account
   end
 
   def show
     string = "date || credit || debit || balance\n"
     @account.transactions.reverse_each do |transaction|
-      string += "#{transaction[0]} || #{transaction[1]} || #{transaction[2]} || #{transaction[3]}\n"
+      if transaction[0].type == "credit"
+      string += credit_display(transaction)
+      else
+      string += debit_display(transaction)
+      end
     end
     string.chomp
+  end
+
+  private
+
+  def credit_display(transaction)
+    "#{transaction[0].date} || #{transaction[0].amount} ||  || #{transaction[0].balance}\n"
+  end
+
+  def debit_display(transaction)
+    "#{transaction[0].date} ||  || #{transaction[0].amount} || #{transaction[0].balance}\n"
   end
 end
